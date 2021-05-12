7df27afd83727564307e74b6363b9b10e5c9f4d6
alinne <alinnegraziellecontato@gmail.com>
2021-05-12 10:45:44 -0300

    second exercise

---
+++ twoNum.py
@@ -0,0 +1,9 @@
+num1 = int(input('Please enter a number: '))
+num2 = int(input('Enter another number: '))
+
+if num1 > num2:
+    print('The first value is higher!')
+elif num2 > num1:
+    print('The second value is higher!!')
+else:
+    print('There isn\'t higher value, the two are the same!!!')
