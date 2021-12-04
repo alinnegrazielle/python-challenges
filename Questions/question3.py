c1cbe87a04eae29707613ed3d5fe16156cc9cb0e
alinnegrazielle <alinnegraziellecontato@gmail.com>
2021-12-04 17:44:28 -0300

    created formatted matrix

--- Questions/question3.py
+++ Questions/question3.py
@@ -1,16 +1,18 @@
-def showMatrix(matrix):
-    for l in matrix:
-        print(l)
 
+'''Ainda falta criar funções!!!'''
+matrix = []
 order = int(input("Qual a ordem da matriz? "))
 
-matrix = []
+for l in range(order):
+    m = []
+    for c in range(order):
+        c = int(input("Digite valor para linha "+str(l)+" e coluna"+str(c)+" "))
+        m.append(c)
+    print()
+    matrix.append(m)
+
 for l in range(order):
     for c in range(order):
-        matrix.insert(c,order)
-        
-        
-print('*')
+        print(matrix[l][c], end=" ")
+    print()
 
-
-showMatrix(matrix)
