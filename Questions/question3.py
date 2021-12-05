a1a5132d52b246d91c0177c30952b4e263eaba32
alinnegrazielle <alinnegraziellecontato@gmail.com>
2021-12-05 12:42:05 -0300

    completed

--- Questions/question3.py
+++ Questions/question3.py
@@ -1,18 +1,21 @@
 
-'''Ainda falta criar funções!!!'''
 matrix = []
 order = int(input("Qual a ordem da matriz? "))
 
-for l in range(order):
-    m = []
-    for c in range(order):
-        c = int(input("Digite valor para linha "+str(l)+" e coluna"+str(c)+" "))
-        m.append(c)
-    print()
-    matrix.append(m)
+def preencher(o):
+    for l in range(o):
+        m = []
+        for c in range(o):
+            c = int(input("Digite valor para linha "+str(l)+" e coluna "+str(c)+": "))
+            m.append(c)
+        print()
+        matrix.append(m)
 
-for l in range(order):
-    for c in range(order):
-        print(matrix[l][c], end=" ")
-    print()
+def mostrar(o):
+    for l in range(o):
+        for c in range(o):
+            print(matrix[l][c], end=" ")
+        print()
 
+preencher(order)
+mostrar(order)
